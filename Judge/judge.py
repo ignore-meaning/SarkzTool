c932b51d794f2a126d4279374ffbf833989b5afc
LemonAI <147178284+LemonServer@users.noreply.github.com>
2025-02-11 17:51:57 +0800

    New Judge

--- Judge/judge.py
+++ Judge/judge.py
@@ -1,12 +1,12 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+from common import Page
+class MainPage(Page.Page):
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
-class MainPage(QWidget):
-    def __init__(self):
-        super().__init__()
-        # 原MainWindow1的UI初始化代码
-        self.label1 = QLabel("Judge")
-        layout = QVBoxLayout()
-        layout.addWidget(self.label1)
-        self.setLayout(layout)
+    def output(self):
+        print(Page.Page.selected_operators)
+
+
